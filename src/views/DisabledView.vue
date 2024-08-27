458f214826dff2bfadfcf8c0018efa486c642938
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-08-27 16:48:47 +0000

    Update disabled view

--- src/views/DisabledView.vue
+++ src/views/DisabledView.vue
@@ -6,8 +6,12 @@ const sessionStore = useSessionStore()
 </script>
 
 <template>
-  <div class="container">
-    <h1>This account is disabled</h1>
+  <div class="container mt-3">
+    <div class="alert alert-danger" role="alert">
+      <h3 class="alert-heading">This account is disabled: {{ sessionStore.username }}</h3>
+      <hr>
+      <p class="mb-0">Please contact an administrator.</p>
+    </div>
     <button @click="sessionStore.logout" class="btn btn-primary" type="button">Logout</button>
   </div>
 </template>
