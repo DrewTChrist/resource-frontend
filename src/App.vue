c7ed23e7fa4bea83bdfa82753394edba4d9653ed
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-08-29 00:42:47 +0000

    Update navbar to use dropdowns

--- src/App.vue
+++ src/App.vue
@@ -20,33 +20,74 @@ watch(
       <nav v-if="$route.meta.showNavbar" class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">Resource Viewer</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <RouterLink class="nav-link" aria-current="page" href="#" to="/home">Home</RouterLink>
+                <RouterLink class="nav-link" aria-current="page" href="#" to="/home"
+                  >Home</RouterLink
+                >
               </li>
               <li class="nav-item">
                 <RouterLink class="nav-link" href="#" to="/grid/1">Grid</RouterLink>
               </li>
-              <li v-if="showManagement" class="nav-item">
+              <!-- <li v-if="showManagement" class="nav-item">
                 <RouterLink class="nav-link" href="#" to="/management">Management</RouterLink>
+              </li> -->
+              <li v-if="showManagement" class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Management
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <RouterLink class="dropdown-item" href="#" to="/management">Users</RouterLink>
+                  </li>
+                  <li><a class="dropdown-item" href="#">Content</a></li>
+                  <li><a class="dropdown-item" href="#">Metadata</a></li>
+                </ul>
               </li>
               <li class="nav-item">
                 <RouterLink class="nav-link" href="#" to="/about">About</RouterLink>
               </li>
-              <li class="nav-item">
-                <RouterLink class="nav-link" href="#" to="/login" @click="sessionStore.logout">Logout</RouterLink>
-              </li>
             </ul>
-            <!--<form v-if="$route.name == 'grid'" class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>-->
+            <div class="d-flex nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                {{ sessionStore.username }}
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <RouterLink
+                    class="dropdown-item"
+                    href="#"
+                    to="/login"
+                    @click="sessionStore.logout"
+                    >Logout
+                  </RouterLink>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
